29c17b3d33fe23570bd5b9ad882005de2b10b16a
awmccanna <a.mccanna@gmail.com>
2017-10-31 20:00:36 -0700

    A nearly fully working board

--- TicTacToe/Form1.Designer.cs
+++ TicTacToe/Form1.Designer.cs
@@ -40,26 +40,26 @@
 			this.btnReset = new System.Windows.Forms.Button();
 			this.txtOut = new System.Windows.Forms.TextBox();
 			this.pictureBox1 = new System.Windows.Forms.PictureBox();
-			this.pictureBox3 = new System.Windows.Forms.PictureBox();
-			this.pictureBox2 = new System.Windows.Forms.PictureBox();
-			this.pictureBox4 = new System.Windows.Forms.PictureBox();
-			this.pictureBox5 = new System.Windows.Forms.PictureBox();
-			this.pictureBox6 = new System.Windows.Forms.PictureBox();
-			this.pictureBox7 = new System.Windows.Forms.PictureBox();
-			this.pictureBox8 = new System.Windows.Forms.PictureBox();
-			this.pictureBox9 = new System.Windows.Forms.PictureBox();
-			this.pictureBox10 = new System.Windows.Forms.PictureBox();
+			this.picUL = new System.Windows.Forms.PictureBox();
+			this.picUM = new System.Windows.Forms.PictureBox();
+			this.picUR = new System.Windows.Forms.PictureBox();
+			this.picML = new System.Windows.Forms.PictureBox();
+			this.picMM = new System.Windows.Forms.PictureBox();
+			this.picMR = new System.Windows.Forms.PictureBox();
+			this.picLL = new System.Windows.Forms.PictureBox();
+			this.picLM = new System.Windows.Forms.PictureBox();
+			this.picLR = new System.Windows.Forms.PictureBox();
 			this.menuStrip1.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.picUL)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.picUM)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.picUR)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.picML)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.picMM)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.picMR)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.picLL)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.picLM)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.picLR)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// menuStrip1
@@ -85,13 +85,13 @@
 			// startGameToolStripMenuItem
 			// 
 			this.startGameToolStripMenuItem.Name = "startGameToolStripMenuItem";
-			this.startGameToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.startGameToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
 			this.startGameToolStripMenuItem.Text = "&Start Game";
 			// 
 			// exitToolStripMenuItem
 			// 
 			this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-			this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.exitToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
 			this.exitToolStripMenuItem.Text = "&Exit";
 			this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
 			// 
@@ -109,13 +109,13 @@
             this.optionOnePlayer,
             this.optionTwoPlayers});
 			this.playersToolStripMenuItem.Name = "playersToolStripMenuItem";
-			this.playersToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.playersToolStripMenuItem.Size = new System.Drawing.Size(111, 22);
 			this.playersToolStripMenuItem.Text = "Pla&yers";
 			// 
 			// optionOnePlayer
 			// 
 			this.optionOnePlayer.Name = "optionOnePlayer";
-			this.optionOnePlayer.Size = new System.Drawing.Size(152, 22);
+			this.optionOnePlayer.Size = new System.Drawing.Size(136, 22);
 			this.optionOnePlayer.Text = "One player";
 			this.optionOnePlayer.Click += new System.EventHandler(this.optionOnePlayer_Click);
 			// 
@@ -124,13 +124,13 @@
 			this.optionTwoPlayers.Checked = true;
 			this.optionTwoPlayers.CheckState = System.Windows.Forms.CheckState.Checked;
 			this.optionTwoPlayers.Name = "optionTwoPlayers";
-			this.optionTwoPlayers.Size = new System.Drawing.Size(152, 22);
+			this.optionTwoPlayers.Size = new System.Drawing.Size(136, 22);
 			this.optionTwoPlayers.Text = "Two players";
 			this.optionTwoPlayers.Click += new System.EventHandler(this.optionTwoPlayers_Click);
 			// 
 			// btnStart
 			// 
-			this.btnStart.Location = new System.Drawing.Point(216, 498);
+			this.btnStart.Location = new System.Drawing.Point(305, 521);
 			this.btnStart.Name = "btnStart";
 			this.btnStart.Size = new System.Drawing.Size(80, 28);
 			this.btnStart.TabIndex = 2;
@@ -140,27 +140,30 @@
 			// 
 			// btnReset
 			// 
-			this.btnReset.Location = new System.Drawing.Point(302, 498);
+			this.btnReset.Enabled = false;
+			this.btnReset.Location = new System.Drawing.Point(391, 521);
 			this.btnReset.Name = "btnReset";
 			this.btnReset.Size = new System.Drawing.Size(81, 28);
 			this.btnReset.TabIndex = 3;
 			this.btnReset.Text = "Reset";
 			this.btnReset.UseVisualStyleBackColor = true;
+			this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
 			// 
 			// txtOut
 			// 
-			this.txtOut.BackColor = System.Drawing.SystemColors.Control;
+			this.txtOut.BackColor = System.Drawing.Color.Honeydew;
 			this.txtOut.BorderStyle = System.Windows.Forms.BorderStyle.None;
 			this.txtOut.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.txtOut.Location = new System.Drawing.Point(9, 472);
+			this.txtOut.Location = new System.Drawing.Point(9, 480);
 			this.txtOut.Multiline = true;
 			this.txtOut.Name = "txtOut";
-			this.txtOut.Size = new System.Drawing.Size(222, 54);
+			this.txtOut.Size = new System.Drawing.Size(262, 69);
 			this.txtOut.TabIndex = 4;
 			this.txtOut.Text = "Press Start to begin game.";
 			// 
 			// pictureBox1
 			// 
+			this.pictureBox1.BackColor = System.Drawing.Color.Honeydew;
 			this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
 			this.pictureBox1.Location = new System.Drawing.Point(42, 42);
 			this.pictureBox1.Name = "pictureBox1";
@@ -168,102 +171,139 @@
 			this.pictureBox1.TabIndex = 5;
 			this.pictureBox1.TabStop = false;
 			// 
-			// pictureBox3
+			// picUL
 			// 
-			this.pictureBox3.Location = new System.Drawing.Point(67, 67);
-			this.pictureBox3.Margin = new System.Windows.Forms.Padding(5);
-			this.pictureBox3.Name = "pictureBox3";
-			this.pictureBox3.Size = new System.Drawing.Size(100, 100);
-			this.pictureBox3.TabIndex = 7;
-			this.pictureBox3.TabStop = false;
+			this.picUL.BackColor = System.Drawing.Color.BlanchedAlmond;
+			this.picUL.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.picUL.Location = new System.Drawing.Point(67, 67);
+			this.picUL.Margin = new System.Windows.Forms.Padding(5);
+			this.picUL.Name = "picUL";
+			this.picUL.Size = new System.Drawing.Size(100, 100);
+			this.picUL.TabIndex = 7;
+			this.picUL.TabStop = false;
+			this.picUL.Visible = false;
+			this.picUL.Click += new System.EventHandler(this.picUL_Click);
 			// 
-			// pictureBox2
+			// picUM
 			// 
-			this.pictureBox2.Location = new System.Drawing.Point(192, 67);
-			this.pictureBox2.Margin = new System.Windows.Forms.Padding(5);
-			this.pictureBox2.Name = "pictureBox2";
-			this.pictureBox2.Size = new System.Drawing.Size(100, 100);
-			this.pictureBox2.TabIndex = 8;
-			this.pictureBox2.TabStop = false;
+			this.picUM.BackColor = System.Drawing.Color.BlanchedAlmond;
+			this.picUM.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.picUM.Location = new System.Drawing.Point(192, 67);
+			this.picUM.Margin = new System.Windows.Forms.Padding(5);
+			this.picUM.Name = "picUM";
+			this.picUM.Size = new System.Drawing.Size(100, 100);
+			this.picUM.TabIndex = 8;
+			this.picUM.TabStop = false;
+			this.picUM.Visible = false;
+			this.picUM.Click += new System.EventHandler(this.picUM_Click);
 			// 
-			// pictureBox4
+			// picUR
 			// 
-			this.pictureBox4.Location = new System.Drawing.Point(317, 67);
-			this.pictureBox4.Margin = new System.Windows.Forms.Padding(5);
-			this.pictureBox4.Name = "pictureBox4";
-			this.pictureBox4.Size = new System.Drawing.Size(100, 100);
-			this.pictureBox4.TabIndex = 9;
-			this.pictureBox4.TabStop = false;
+			this.picUR.BackColor = System.Drawing.Color.BlanchedAlmond;
+			this.picUR.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.picUR.Location = new System.Drawing.Point(317, 67);
+			this.picUR.Margin = new System.Windows.Forms.Padding(5);
+			this.picUR.Name = "picUR";
+			this.picUR.Size = new System.Drawing.Size(100, 100);
+			this.picUR.TabIndex = 9;
+			this.picUR.TabStop = false;
+			this.picUR.Visible = false;
+			this.picUR.Click += new System.EventHandler(this.picUR_Click);
 			// 
-			// pictureBox5
+			// picML
 			// 
-			this.pictureBox5.Location = new System.Drawing.Point(67, 192);
-			this.pictureBox5.Margin = new System.Windows.Forms.Padding(5);
-			this.pictureBox5.Name = "pictureBox5";
-			this.pictureBox5.Size = new System.Drawing.Size(100, 100);
-			this.pictureBox5.TabIndex = 10;
-			this.pictureBox5.TabStop = false;
+			this.picML.BackColor = System.Drawing.Color.BlanchedAlmond;
+			this.picML.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.picML.Location = new System.Drawing.Point(67, 192);
+			this.picML.Margin = new System.Windows.Forms.Padding(5);
+			this.picML.Name = "picML";
+			this.picML.Size = new System.Drawing.Size(100, 100);
+			this.picML.TabIndex = 10;
+			this.picML.TabStop = false;
+			this.picML.Visible = false;
+			this.picML.Click += new System.EventHandler(this.picML_Click);
 			// 
-			// pictureBox6
+			// picMM
 			// 
-			this.pictureBox6.Location = new System.Drawing.Point(192, 192);
-			this.pictureBox6.Margin = new System.Windows.Forms.Padding(5);
-			this.pictureBox6.Name = "pictureBox6";
-			this.pictureBox6.Size = new System.Drawing.Size(100, 100);
-			this.pictureBox6.TabIndex = 11;
-			this.pictureBox6.TabStop = false;
+			this.picMM.BackColor = System.Drawing.Color.BlanchedAlmond;
+			this.picMM.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.picMM.Location = new System.Drawing.Point(192, 192);
+			this.picMM.Margin = new System.Windows.Forms.Padding(5);
+			this.picMM.Name = "picMM";
+			this.picMM.Size = new System.Drawing.Size(100, 100);
+			this.picMM.TabIndex = 11;
+			this.picMM.TabStop = false;
+			this.picMM.Visible = false;
+			this.picMM.Click += new System.EventHandler(this.picMM_Click);
 			// 
-			// pictureBox7
+			// picMR
 			// 
-			this.pictureBox7.Location = new System.Drawing.Point(317, 192);
-			this.pictureBox7.Margin = new System.Windows.Forms.Padding(5);
-			this.pictureBox7.Name = "pictureBox7";
-			this.pictureBox7.Size = new System.Drawing.Size(100, 100);
-			this.pictureBox7.TabIndex = 12;
-			this.pictureBox7.TabStop = false;
+			this.picMR.BackColor = System.Drawing.Color.BlanchedAlmond;
+			this.picMR.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.picMR.Location = new System.Drawing.Point(317, 192);
+			this.picMR.Margin = new System.Windows.Forms.Padding(5);
+			this.picMR.Name = "picMR";
+			this.picMR.Size = new System.Drawing.Size(100, 100);
+			this.picMR.TabIndex = 12;
+			this.picMR.TabStop = false;
+			this.picMR.Visible = false;
+			this.picMR.Click += new System.EventHandler(this.picMR_Click);
 			// 
-			// pictureBox8
+			// picLL
 			// 
-			this.pictureBox8.Location = new System.Drawing.Point(67, 317);
-			this.pictureBox8.Margin = new System.Windows.Forms.Padding(5);
-			this.pictureBox8.Name = "pictureBox8";
-			this.pictureBox8.Size = new System.Drawing.Size(100, 100);
-			this.pictureBox8.TabIndex = 13;
-			this.pictureBox8.TabStop = false;
+			this.picLL.BackColor = System.Drawing.Color.BlanchedAlmond;
+			this.picLL.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.picLL.Location = new System.Drawing.Point(67, 317);
+			this.picLL.Margin = new System.Windows.Forms.Padding(5);
+			this.picLL.Name = "picLL";
+			this.picLL.Size = new System.Drawing.Size(100, 100);
+			this.picLL.TabIndex = 13;
+			this.picLL.TabStop = false;
+			this.picLL.Visible = false;
+			this.picLL.Click += new System.EventHandler(this.picLL_Click);
 			// 
-			// pictureBox9
+			// picLM
 			// 
-			this.pictureBox9.Location = new System.Drawing.Point(192, 317);
-			this.pictureBox9.Margin = new System.Windows.Forms.Padding(5);
-			this.pictureBox9.Name = "pictureBox9";
-			this.pictureBox9.Size = new System.Drawing.Size(100, 100);
-			this.pictureBox9.TabIndex = 14;
-			this.pictureBox9.TabStop = false;
+			this.picLM.BackColor = System.Drawing.Color.BlanchedAlmond;
+			this.picLM.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.picLM.Location = new System.Drawing.Point(192, 317);
+			this.picLM.Margin = new System.Windows.Forms.Padding(5);
+			this.picLM.Name = "picLM";
+			this.picLM.Size = new System.Drawing.Size(100, 100);
+			this.picLM.TabIndex = 14;
+			this.picLM.TabStop = false;
+			this.picLM.Visible = false;
+			this.picLM.Click += new System.EventHandler(this.picLM_Click);
 			// 
-			// pictureBox10
+			// picLR
 			// 
-			this.pictureBox10.Location = new System.Drawing.Point(317, 317);
-			this.pictureBox10.Margin = new System.Windows.Forms.Padding(5);
-			this.pictureBox10.Name = "pictureBox10";
-			this.pictureBox10.Size = new System.Drawing.Size(100, 100);
-			this.pictureBox10.TabIndex = 15;
-			this.pictureBox10.TabStop = false;
+			this.picLR.BackColor = System.Drawing.Color.BlanchedAlmond;
+			this.picLR.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.picLR.Location = new System.Drawing.Point(317, 317);
+			this.picLR.Margin = new System.Windows.Forms.Padding(5);
+			this.picLR.Name = "picLR";
+			this.picLR.Size = new System.Drawing.Size(100, 100);
+			this.picLR.TabIndex = 15;
+			this.picLR.TabStop = false;
+			this.picLR.Visible = false;
+			this.picLR.Click += new System.EventHandler(this.picLR_Click);
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.BackColor = System.Drawing.Color.Honeydew;
 			this.ClientSize = new System.Drawing.Size(484, 561);
-			this.Controls.Add(this.pictureBox10);
-			this.Controls.Add(this.pictureBox9);
-			this.Controls.Add(this.pictureBox8);
-			this.Controls.Add(this.pictureBox7);
-			this.Controls.Add(this.pictureBox6);
-			this.Controls.Add(this.pictureBox5);
-			this.Controls.Add(this.pictureBox4);
-			this.Controls.Add(this.pictureBox2);
-			this.Controls.Add(this.pictureBox3);
+			this.Controls.Add(this.picLR);
+			this.Controls.Add(this.picLM);
+			this.Controls.Add(this.picLL);
+			this.Controls.Add(this.picMR);
+			this.Controls.Add(this.picMM);
+			this.Controls.Add(this.picML);
+			this.Controls.Add(this.picUR);
+			this.Controls.Add(this.picUM);
+			this.Controls.Add(this.picUL);
 			this.Controls.Add(this.pictureBox1);
 			this.Controls.Add(this.txtOut);
 			this.Controls.Add(this.btnReset);
@@ -275,15 +315,15 @@
 			this.menuStrip1.ResumeLayout(false);
 			this.menuStrip1.PerformLayout();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.picUL)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.picUM)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.picUR)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.picML)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.picMM)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.picMR)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.picLL)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.picLM)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.picLR)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -303,15 +343,15 @@
 		private System.Windows.Forms.Button btnReset;
 		private System.Windows.Forms.TextBox txtOut;
 		private System.Windows.Forms.PictureBox pictureBox1;
-		private System.Windows.Forms.PictureBox pictureBox3;
-		private System.Windows.Forms.PictureBox pictureBox2;
-		private System.Windows.Forms.PictureBox pictureBox4;
-		private System.Windows.Forms.PictureBox pictureBox5;
-		private System.Windows.Forms.PictureBox pictureBox6;
-		private System.Windows.Forms.PictureBox pictureBox7;
-		private System.Windows.Forms.PictureBox pictureBox8;
-		private System.Windows.Forms.PictureBox pictureBox9;
-		private System.Windows.Forms.PictureBox pictureBox10;
+		private System.Windows.Forms.PictureBox picUL;
+		private System.Windows.Forms.PictureBox picUM;
+		private System.Windows.Forms.PictureBox picUR;
+		private System.Windows.Forms.PictureBox picML;
+		private System.Windows.Forms.PictureBox picMM;
+		private System.Windows.Forms.PictureBox picMR;
+		private System.Windows.Forms.PictureBox picLL;
+		private System.Windows.Forms.PictureBox picLM;
+		private System.Windows.Forms.PictureBox picLR;
 	}
 }
 
--- TicTacToe/Form1.cs
+++ TicTacToe/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,6 +14,13 @@ namespace TicTacToe
 	public partial class Form1 : Form
 	{
 		private System.Drawing.Graphics g;
+		private Boolean playerOneTurn;
+		
+		private PictureBox[] boxes = new PictureBox[9];//box order is UL, UM, UR, ML, MM, MR, LL, LM, LR
+
+		private int[][] grid = new int[3][];
+
+
 		public Form1()
 		{
 			InitializeComponent();
@@ -38,14 +46,40 @@ namespace TicTacToe
 
 		private void btnStart_Click(object sender, EventArgs e)
 		{
+			for (int i = 0; i < 3; i++)
+			{
+				grid[i] = new int[3];
+			}
+
+			DrawGameBoard();
+			ResetBoxes();
 			this.StartGame();
+		}
+
+		private void ResetBoxes()
+		{
+			
+			for (int i = 0; i < 3; i++)
+			{
+				for (int j = 0; j < 3; j++)
+				{
+					grid[i][j] = -1;
+				}
+			}
+
+			foreach(PictureBox p in boxes)
+			{
+				p.Image = null;
+				p.Invalidate();
+			}
+
 		}
 
 		private void StartGame()
 		{
+			btnReset.Enabled = true;
+			btnStart.Enabled = false;
 			Boolean twoPlayers = this.optionTwoPlayers.Checked;
-			Boolean winner = false;
-			Boolean playerOneTurn;
 
 			if(!twoPlayers)
 			{
@@ -53,7 +87,6 @@ namespace TicTacToe
 			}
 			else
 			{
-				DrawGameBoard();
 				int goesFirst = ChooseFirstPlayer();
 				if(goesFirst == 1)
 				{
@@ -66,35 +99,22 @@ namespace TicTacToe
 					MessageBox.Show("Player two goes first.");
 				}
 
-				while(!winner)
-				{
-
-					if(playerOneTurn)
-					{
-						PlayerOneGo();
-					}
-					else
-					{
-						PlayerTwoGo();
-					}
-
-
-
-
-
-					winner = CheckForWinner();
-
-				}
-
-
-
+				DisplayPlayerTurn();
 			}
 
 
+		}
 
-
-
-
+		private void DisplayPlayerTurn()
+		{
+			if (playerOneTurn)
+			{
+				PlayerOneGo();
+			}
+			else
+			{
+				PlayerTwoGo();
+			}
 		}
 
 		private void PlayerTwoGo()
@@ -107,9 +127,65 @@ namespace TicTacToe
 			this.txtOut.Text = "Player Ones turn to go!";
 		}
 
-		private bool CheckForWinner()
+		private Boolean CheckForWinner()
 		{
-			return true;
+			for(int i = 0; i < 3; i++)
+			{
+				if(grid[i][0] > 0 && grid[i][0] == grid[i][1] && grid[i][0] == grid[i][2])
+				{
+					return true;
+				}
+				if(grid[0][i] > 0 && grid[0][i] == grid[1][i] && grid[0][i] == grid[2][i])
+				{
+					return true;
+				}
+
+			}
+			if (grid[1][1] > 0 && grid[0][0] == grid[1][1] && grid[1][1] == grid[2][2])
+			{
+				return true;
+			}
+			if (grid[1][1] > 0 && grid[2][0] == grid[1][1] && grid[1][1] == grid[0][2])
+			{
+				return true;
+			}
+
+			return false;
+		}
+
+		private void DrawGameBoard()
+		{
+			System.Drawing.Pen boardPen = new System.Drawing.Pen(Color.ForestGreen, 20);
+			g = pictureBox1.CreateGraphics();
+			g.DrawLine(boardPen, 137, 20, 137, 380);
+			g.DrawLine(boardPen, 262, 20, 262, 380);
+			g.DrawLine(boardPen, 20, 137, 380, 137);
+			g.DrawLine(boardPen, 20, 262, 380, 262);
+
+			this.picLL.Visible = true;
+			this.picLM.Visible = true;
+			this.picLR.Visible = true;
+			this.picML.Visible = true;
+			this.picMM.Visible = true;
+			this.picMR.Visible = true;
+			this.picUL.Visible = true;
+			this.picUM.Visible = true;
+			this.picUR.Visible = true;
+
+
+			boxes[0] = picUL;
+			boxes[1] = picUM;
+			boxes[2] = picUR;
+
+			boxes[3] = picML;
+			boxes[4] = picMM;
+			boxes[5] = picMR;
+
+			boxes[6] = picLL;
+			boxes[7] = picLM;
+			boxes[8] = picLR;
+
+
 		}
 
 		private int ChooseFirstPlayer()
@@ -118,16 +194,146 @@ namespace TicTacToe
 			return (rnd.Next(100) % 2)+1;
 		}
 
-		private void DrawGameBoard()
-		{
-			System.Drawing.Pen boardPen = new System.Drawing.Pen(Color.Black, 6);
-			g = pictureBox1.CreateGraphics();
-			g.DrawLine(boardPen, 162, 0, 162, 379);
-		}
-
 		private void exitToolStripMenuItem_Click(object sender, EventArgs e)
 		{
 			Close();
 		}
+
+#region pictureBoxClicks
+		private void picUL_Click(object sender, EventArgs e)
+		{
+			DrawOnBoard(sender);
+			UpdateGrid(0, 0);
+		}
+
+		private void picUM_Click(object sender, EventArgs e)
+		{
+			DrawOnBoard(sender);
+			UpdateGrid(0, 1);
+		}
+
+		private void picUR_Click(object sender, EventArgs e)
+		{
+			UpdateGrid(0, 2);
+			DrawOnBoard(sender);
+		}
+
+		private void picML_Click(object sender, EventArgs e)
+		{
+			UpdateGrid(1, 0);
+			DrawOnBoard(sender);
+		}
+
+		private void picMM_Click(object sender, EventArgs e)
+		{
+			UpdateGrid(1, 1);
+			DrawOnBoard(sender);
+
+		}
+
+		private void picMR_Click(object sender, EventArgs e)
+		{
+			UpdateGrid(1, 2);
+			DrawOnBoard(sender);
+
+		}
+
+		private void picLL_Click(object sender, EventArgs e)
+		{
+			UpdateGrid(2, 0);
+			DrawOnBoard(sender);
+
+		}
+
+		private void picLM_Click(object sender, EventArgs e)
+		{
+			UpdateGrid(2, 1);
+			DrawOnBoard(sender);
+
+		}
+
+		private void picLR_Click(object sender, EventArgs e)
+		{
+			UpdateGrid(2, 2);
+			DrawOnBoard(sender);
+		}
+		#endregion
+
+		private void UpdateGrid(int v1, int v2)
+		{
+			if(playerOneTurn)
+			{
+				grid[v1][v2] = 1;
+			}
+			else
+			{
+				grid[v1][v2] = 2;
+			}
+		}
+
+		private void DrawOnBoard(object sender)
+		{
+			
+			if (this.playerOneTurn)
+			{
+				DrawX(sender);
+			}
+			else
+			{
+				DrawO(sender);
+			}
+
+			if(!CheckForWinner())
+			{
+				NextPlayer();
+			}
+			else
+			{
+				if(playerOneTurn)
+				{
+					MessageBox.Show("Player One wins! Congratulations!");
+				}
+				else
+				{
+					MessageBox.Show("Player Two wins! Congratulations!");
+				}
+			}
+
+			
+		}
+
+		private void NextPlayer()
+		{
+			playerOneTurn = !playerOneTurn;
+			DisplayPlayerTurn();
+		}
+
+		private void DrawO(object sender)
+		{
+			System.Drawing.Pen oPen = new System.Drawing.Pen(Color.Blue, 10);
+			PictureBox toDraw = (PictureBox)sender;
+			g = toDraw.CreateGraphics();
+			g.DrawEllipse(oPen, 8, 8, toDraw.Width - 20, toDraw.Width - 20);
+		}
+
+		private void DrawX(object sender)
+		{
+			System.Drawing.Pen xPen = new System.Drawing.Pen(Color.Red, 10);
+			PictureBox toDraw = (PictureBox)sender;
+			g = toDraw.CreateGraphics();
+			g.DrawLine(xPen, 10, 10, toDraw.Width-20, toDraw.Width - 20);
+			g.DrawLine(xPen, 10, toDraw.Width - 20, toDraw.Width - 20, 10);
+			
+
+		}
+
+		private void btnReset_Click(object sender, EventArgs e)
+		{
+			ResetBoxes();
+			btnReset.Enabled = false;
+			btnStart.Enabled = true;
+		}
 	}
+	
+
 }
