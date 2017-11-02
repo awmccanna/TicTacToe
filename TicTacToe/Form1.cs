1ffd34c8495ccb38f9c70076666f695b29e4e97d
awmccanna <a.mccanna@gmail.com>
2017-11-01 22:41:58 -0700

    Actually it's mostly done now? Added "AI"

--- TicTacToe/Form1.cs
+++ TicTacToe/Form1.cs
@@ -14,7 +14,7 @@ namespace TicTacToe
 	public partial class Form1 : Form
 	{
 		private System.Drawing.Graphics g;
-		private Boolean playerOneTurn;
+		private Boolean playerOneTurn, twoPlayers, gameInProgress;
 		
 		private PictureBox[] boxes = new PictureBox[9];//box order is UL, UM, UR, ML, MM, MR, LL, LM, LR
 
@@ -93,31 +93,29 @@ namespace TicTacToe
 
 		private void StartGame()
 		{
-			Boolean twoPlayers = this.optionTwoPlayers.Checked;
-
-			if (!twoPlayers)
+			twoPlayers = this.optionTwoPlayers.Checked;
+			gameInProgress = true;
+			if(!twoPlayers)
 			{
-				MessageBox.Show("Sorry, we don't have a computer opponent up and running yet!");
+				MessageBox.Show("The computer will be Player Two");
+			}
+
+			btnReset.Enabled = true;
+			btnStart.Enabled = false;
+				
+			int goesFirst = ChooseFirstPlayer();
+			if(goesFirst == 1)
+			{
+				playerOneTurn = true;
+				MessageBox.Show("Player one goes first.", "Coin Flip");
 			}
 			else
 			{
-				btnReset.Enabled = true;
-				btnStart.Enabled = false;
-				
-				int goesFirst = ChooseFirstPlayer();
-				if(goesFirst == 1)
-				{
-					playerOneTurn = true;
-					MessageBox.Show("Player one goes first.", "Coin Flip");
-				}
-				else
-				{
-					playerOneTurn = false;
-					MessageBox.Show("Player two goes first.", "Coin Flip");
-				}
-
-				DisplayPlayerTurn();
+				playerOneTurn = false;
+				MessageBox.Show("Player two goes first.", "Coin Flip");
 			}
+
+			DisplayPlayerTurn();
 
 
 		}
@@ -137,16 +135,228 @@ namespace TicTacToe
 				PlayerTwoGo();
 			}
 		}
-
-		private void PlayerTwoGo()
-		{
-			this.txtOut.Text = "Player Twos turn to go!";
-		}
-
 		private void PlayerOneGo()
 		{
 			this.txtOut.Text = "Player Ones turn to go!";
 		}
+		private void PlayerTwoGo()
+		{
+			this.txtOut.Text = "Player Twos turn to go!";
+			if(!twoPlayers)
+			{
+				ComputerTurn();
+			}
+		}
+
+		/*
+		 * This is the code for the "AI"
+		 * It will be ugly
+		 */
+		private void ComputerTurn()
+		{
+
+			for (int i = 0; i < 3; i++)
+			{
+				//First check is to see if any columns are going to be filled by either person. Will block or win
+				if (grid[i][0] < 0 && grid[i][1] == grid[i][2])//Is top not filled, but bottom two are
+				{
+					if(i == 0 && picUL.Enabled)
+					{
+						picUL_Click(picUL, null);//Upper left box
+						return;
+					}
+					else if( i == 1 && picUM.Enabled)
+					{
+						picUM_Click(picUM, null);//Upper middle box
+						return;
+					}
+					else if(i == 2 && picUR.Enabled)
+					{
+						picUR_Click(picUR, null);//Upper right box. Told you it would be ugly
+						return;
+					}
+				}
+				else if(grid[i][1] < 0 && grid[i][0] == grid[i][2])//Is middle empty, but top and bottom are filled
+				{
+					if (i == 0 && picML.Enabled)
+					{
+						picML_Click(picML, null);//Middle left box
+						return;
+					}
+					else if (i == 1 && picMM.Enabled)
+					{
+						picMM_Click(picMM, null);//Middle middle box
+						return;
+					}
+					else if (i == 2 && picMR.Enabled)
+					{
+						picMR_Click(picMR, null);//Middle right box
+						return;
+					}
+				}
+				else if(grid[i][2] < 0 && grid[i][1] == grid[i][0])//Is bottom empty but top two aren't
+				{
+					if (i == 0 && picLL.Enabled)
+					{
+						picLL_Click(picLL, null);//Lower left box
+						return;
+					}
+					else if (i == 1 && picLM.Enabled)
+					{
+						picLM_Click(picLM, null);//Lower middle box
+						return;
+					}
+					else if (i == 2 && picLR.Enabled)
+					{
+						picLR_Click(picLR, null);//Lower right box
+						return;
+					}
+				}
+
+
+
+
+				//Now check if any of the rows are about to be filled
+				if (grid[0][i] < 0 && grid[1][i] == grid[2][i])//Left box not filled
+				{
+					if (i == 0 && picUL.Enabled)
+					{
+						picUL_Click(picUL, null);//Upper left box
+						return;
+					}
+					else if (i == 1 && picML.Enabled)
+					{
+						picML_Click(picML, null);//Middle left box
+						return;
+					}
+					else if (i == 2 && picLL.Enabled)
+					{
+						picLL_Click(picLL, null);//Lower left box
+						return;
+					}
+				}
+				else if (grid[1][i] < 0 && grid[0][i] == grid[2][i])//Middle box not filled
+				{
+					if (i == 0 && picUM.Enabled)
+					{
+						picUM_Click(picUM, null);//Upper Middle box
+						return;
+					}
+					else if (i == 1 && picMM.Enabled)
+					{
+						picMM_Click(picMM, null);//Middle Middle box
+						return;
+					}
+					else if (i == 2 && picLM.Enabled)
+					{
+						picLM_Click(picLM, null);//Lower Middle box
+						return;
+					}
+				}
+				else if (grid[2][i] < 0 && grid[0][i] == grid[1][i])
+				{
+					if (i == 0 && picUR.Enabled)
+					{
+						picUR_Click(picUR, null);//Upper Right box
+						return;
+					}
+					else if (i == 1 && picMR.Enabled)
+					{
+						picMR_Click(picMR, null);//Middle Right box
+						return;
+					}
+					else if (i == 2 && picLR.Enabled)
+					{
+						picLR_Click(picLR, null);//Lower Right box
+						return;
+					}
+				}
+
+			}
+			if(grid[0][0] < 0 && grid[1][1] == grid[2][2] && picUL.Enabled)
+			{
+				picUL_Click(picUL, null);//Upper Left
+				return;
+			}
+			if (grid[1][1] < 0 && grid[0][0] == grid[2][2] && picMM.Enabled)
+			{
+				picMM_Click(picMM, null);//Middle Middle
+				return;
+			}
+			if (grid[1][1] < 0 && grid[0][0] == grid[2][2] && picLR.Enabled)
+			{
+				picLR_Click(picLR, null);//Lower Right
+				return;
+			}
+			if (grid[0][2] < 0 && grid[2][0] == grid[1][1] && picLL.Enabled)
+			{
+				picLL_Click(picLL, null);//Lower left
+				return;
+			}
+			if (grid[1][1] < 0 && grid[2][0] == grid[0][2] && picMM.Enabled)
+			{
+				picMM_Click(picMM, null);//Middle middle
+				return;
+			}
+			if (grid[2][0] < 0 && grid[0][2] == grid[1][1] && picUR.Enabled)
+			{
+				picUR_Click(picUR, null);//Upper right
+				return;
+			}
+
+
+			//Everything above is the "smart" decision making. Everything below here will be the straight brute force stuff.
+			//Even uglier if you can believe it
+
+			if(picMM.Enabled)
+			{
+				picMM_Click(picMM, null);
+				return;
+			}
+			if(picUL.Enabled)
+			{
+				picUL_Click(picUL, null);
+				return;
+			}
+			if (picUM.Enabled)
+			{
+				picUM_Click(picUM, null);
+				return;
+			}
+			if (picLR.Enabled)
+			{
+				picLR_Click(picLR, null);
+				return;
+			}
+			if (picLL.Enabled)
+			{
+				picLL_Click(picLL, null);
+				return;
+			}
+			if (picMR.Enabled)
+			{
+				picMR_Click(picMR, null);
+				return;
+			}
+			if (picUR.Enabled)
+			{
+				picUR_Click(picUR, null);
+				return;
+			}
+			if (picML.Enabled)
+			{
+				picML_Click(picML, null);
+				return;
+			}
+			if (picLM.Enabled)
+			{
+				picLM_Click(picLM, null);
+				return;
+			}
+
+		}
+
+		
 
 		private Boolean CheckForWinner()
 		{
@@ -326,17 +536,20 @@ namespace TicTacToe
 				{
 					MessageBox.Show("Player One wins! Congratulations!");
 					this.txtOut.Text = "Player One wins!";
+					gameInProgress = false;
 				}
 				else
 				{
 					MessageBox.Show("Player Two wins! Congratulations!");
 					this.txtOut.Text = "Player Two wins!";
+					gameInProgress = false;
 				}
 			}
 			else if(CheckForDraw())
 			{
 				MessageBox.Show("It's a draw!");
 				this.txtOut.Text = "Draw!";
+				gameInProgress = false;
 			}
 			else
 			{
